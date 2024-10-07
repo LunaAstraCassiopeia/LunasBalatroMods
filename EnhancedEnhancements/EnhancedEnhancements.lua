95fb1871099e2210acc706b3ac1736d15bcd36b3
Dimserene <31881806+Dimserene@users.noreply.github.com>
2024-10-07 18:28:46 +0800

    Update EnhancedEnhancements.lua

--- EnhancedEnhancements/EnhancedEnhancements.lua
+++ EnhancedEnhancements/EnhancedEnhancements.lua
@@ -7,17 +7,29 @@
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
-function SMODS.INIT.EnhancedEnhancements()
+SMODS.Atlas {
+	key = "centers",
+	path = "Enhancedments.png",
+	px = 71,
+	py = 95,
+	raw_key = true,
+}
 
-    local enhancement_mod = SMODS.findModByID("EnhancedEnhancements")
-    local sprite_enhancer = SMODS.Sprite:new("centers", enhancement_mod.path, "Enhancedments.png", 71, 95, "asset_atli")
-    local card_contrast = SMODS.Sprite:new("cards_2", enhancement_mod.path, "EnhancedContrast.png", 71, 95, "asset_atli")
-    local ui_contrast = SMODS.Sprite:new("ui_2", enhancement_mod.path, "EnhancedUIContrast.png", 18, 18, "asset_atli")
-    
-    sprite_enhancer:register()
-    card_contrast:register()
-    ui_contrast:register()
-end
+SMODS.Atlas{
+    key = "centers",
+    path = "Enhancedments.png",
+    px = 71,
+    py = 95,
+    inject = function(self)
+        SMODS.Atlas.inject(self)
+        G.shared_seals = {
+            Gold = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 2,y = 0}),
+            Purple = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 4,y = 4}),
+            Red = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 5,y = 4}),
+            Blue = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 6,y = 4}),
+        }
+    end
+}
 
 ----------------------------------------------
 ------------MOD CODE END----------------------
