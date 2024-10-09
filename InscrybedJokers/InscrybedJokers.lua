b327a7a5c4fbb2833ebd4a6d810592495ec87f5f
LunaAstraCassiopeia <63694794+LunaAstraCassiopeia@users.noreply.github.com>
2024-10-09 19:54:22 +0800

    Update InscrybedJokers.lua
    fixed deathly joker

--- InscrybedJokers/InscrybedJokers.lua
+++ InscrybedJokers/InscrybedJokers.lua
@@ -129,6 +129,7 @@ function SMODS.INIT.InscrybedJokers()
                 for k, v in ipairs(context.glass_shattered) do
                     value = value + v.base.nominal
                 end
+            self.ability.extra.current_chips = self.ability.extra.current_chips + value
             G.E_MANAGER:add_event(Event({
             func = function() card_eval_status_text(self, 'extra', nil, nil, nil, {message = localize{type = 'variable', key = 'a_chips', vars = {self.ability.extra.current_chips}}}); return true
             end}))
@@ -138,6 +139,7 @@ function SMODS.INIT.InscrybedJokers()
                 for k, v in ipairs(context.removed) do
                     value = value + v.base.nominal
                 end
+            self.ability.extra.current_chips = self.ability.extra.current_chips + value
             G.E_MANAGER:add_event(Event({
             func = function() card_eval_status_text(self, 'extra', nil, nil, nil, {message = localize{type = 'variable', key = 'a_chips', vars = {self.ability.extra.current_chips}}}); return true
             end}))
